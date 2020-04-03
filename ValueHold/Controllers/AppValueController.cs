2ce6e19f357217ae2972745f029ad7ba40a34874
Tienie Jordaan <tieniejordaan@Tienies-MacBook-Pro.local>
2020-04-03 08:18:21 +0800

    initial

---
+++ ValueHold/Controllers/AppValueController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+
+namespace ValueHold.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppValueController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            var path = $"{Directory.GetCurrentDirectory()}/value.txt";
+            return System.IO.File.ReadAllText(path);
+        }
+
+        [HttpPost]
+        public async void Post(string value)
+        {
+            var path = $"{Directory.GetCurrentDirectory()}/value.txt";
+
+            using (StreamWriter writer = System.IO.File.CreateText(path))
+            {
+                writer.Write(value);
+            }
+
+        }
+    }
+}
